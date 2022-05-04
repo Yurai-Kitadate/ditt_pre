97918d71d371a4224cfb6a0f68fdcf9986b4f373
Yurai-Kitadate <yuurai18@gmail.com>
2022-05-04 16:43:33 +0900

    first Commit.

---
+++ Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by yurai on 2022/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            Text("Hello, ditt!")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ditt_preApp.swift
@@ -0,0 +1,17 @@
+//
+//  ditt_preApp.swift
+//  Shared
+//
+//  Created by yurai on 2022/05/04.
+//
+
+import SwiftUI
+
+@main
+struct ditt_preApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
