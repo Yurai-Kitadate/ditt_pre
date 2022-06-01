ee20244c46c13a05fce19a971658dc92bdabb855
Yurai-Kitadate <yuurai18@gmail.com>
2022-06-01 11:40:11 +0900

    detailの文字色をcolorに追加

--- Shared/Color/Color.swift
+++ Shared/Color/Color.swift
@@ -7,4 +7,12 @@
 
 import Foundation
 import SwiftUI
-let myGray : Color = Color(red: 231/255, green: 241/255, blue: 1.0)
+let myGray   : Color = Color(red: 231/255, green: 241/255, blue: 255/255)
+let myYellow : Color = Color(red: 0.975, green: 0.702, blue: 0)
+let myGreen  : Color = Color(red: 4.0/255, green: 220/225, blue: 90/255)
+let myBlue   : Color = Color(red: 33.0/255, green: 149/225, blue: 255/255)
+let myRed    : Color = Color(red: 242.0/255, green: 49.0/255, blue: 130.0/255)
+ 
+
+
+
--- Shared/View/Detail/CategoryTitleView.swift
+++ Shared/View/Detail/CategoryTitleView.swift
@@ -7,7 +7,7 @@ struct CategoryTitleView: View{
             HStack(alignment: .top){
                 Image("design")
                 VStack(alignment: .leading){
-                    Text("Design").foregroundColor(Color(red: 4.0/255, green: 220/225, blue: 90/255))
+                    Text("Design").foregroundColor(myGreen)
                     Text("デザイン")
                 }
             }
@@ -16,7 +16,7 @@ struct CategoryTitleView: View{
             HStack{
                 Image("tech")
                 VStack(alignment: .leading){
-                    Text("Technology").foregroundColor(Color(red: 33.0/255, green: 149/225, blue: 255/255))
+                    Text("Technology").foregroundColor(myBlue)
                     Text("技術")
                 }
             }
@@ -25,7 +25,7 @@ struct CategoryTitleView: View{
             HStack{
                 Image("idea")
                 VStack(alignment: .leading){
-                    Text("Idea").foregroundColor(Color(red: 0.975, green: 0.702, blue: 0))
+                    Text("Idea").foregroundColor(myYellow)
                     Text("アイデア")
                 }
             }
@@ -34,7 +34,7 @@ struct CategoryTitleView: View{
             HStack{
                 Image("team")
                 VStack(alignment: .leading){
-                    Text("Team").foregroundColor(Color(red: 242.0/255, green: 49.0/255, blue: 130.0/255))
+                    Text("Team").foregroundColor(myRed)
                     Text("チーム")
                 }
             }
